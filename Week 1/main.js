58d345ac06525e207a72e1511cffb4f4b3dc75c1
shankure <darkokoprivnjak1@gmail.com>
2025-09-30 16:47:21 +0200

    Add Week 2 and Class Assignment code, also fixing the issue with folders structure

---
+++ Week 1/main.js
@@ -0,0 +1,41 @@
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x202020);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+camera.position.z = 10;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff88 });
+const cubeMesh = new THREE.Mesh(geometry, material);
+scene.add(cubeMesh);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(2, 2, 5);
+scene.add(light);
+
+function animate() {
+  requestAnimationFrame(animate);
+  cubeMesh.rotation.x += 0.01;
+  cubeMesh.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
